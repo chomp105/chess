3c6e203014542735175e76e9777c7fd7a69b601c
chomp105 <54012005+chomp105@users.noreply.github.com>
2021-11-14 11:35:46 -0600

    Delete bin directory

--- bin/chess.h
+++
@@ -1,7 +0,0 @@
-#ifndef CHESS_H
-#define CHESS_H
-
-void print_board(int board[8][8][2], char pieces[2][16][4]);
-int move(int board[8][8][2], int sx, int sy, int ex, int ey, int king[2][2], int player, int *game, int enpas[5]);
-
-#endif //CHESS_H
